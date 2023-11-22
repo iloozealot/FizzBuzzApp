2893bee24308fbe0743683049cab160edd868c17
iloozealot <iloozealot@gmail.com>
2023-11-22 12:56:03 -0600

    Add project files.

---
+++ FizzBuzzUI/Calculations/CalculationsModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzUI.Calculations
+{
+    public class CalculationsModel
+    {
+
+    }
+}
